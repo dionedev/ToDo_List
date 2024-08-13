99f4cf9fab7fbf45979e1b1ea70a4a9945f8bae6
Dione Santos <dionesantos@Diones-MacBook-Pro.local>
2024-08-13 15:52:00 -0300

    feat: adiciona um widget taskcard customizado para exibir a tarefa

---
+++ lib/components/task_card.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatefulWidget {
+  final String name;
+  final String time;
+  final bool isDone;
+
+  const TaskCard(
+      {super.key, required this.name, required this.time, this.isDone = false});
+
+  @override
+  State<TaskCard> createState() => _TaskCardState();
+}
+
+class _TaskCardState extends State<TaskCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      margin: const EdgeInsets.only(bottom: 8),
+      decoration: const BoxDecoration(
+        color: Color.fromRGBO(32, 31, 31, 1),
+        borderRadius: BorderRadius.all(
+          Radius.circular(8),
+        ),
+      ),
+      child: Row(
+        children: [
+          Checkbox(
+            value: widget.isDone,
+            onChanged: (value) {
+              setState(() {});
+            },
+          ),
+          Text(
+            widget.name,
+            style: TextStyle(
+              decoration: widget.isDone
+                  ? TextDecoration.lineThrough
+                  : TextDecoration.none,
+              decorationColor: const Color.fromRGBO(122, 119, 119, 1),
+              decorationThickness: 2,
+              fontSize: 18,
+              color: const Color.fromRGBO(122, 119, 119, 1),
+            ),
+          ),
+          Text(
+            widget.time,
+            style: const TextStyle(
+              fontSize: 14,
+              color: Color.fromRGBO(122, 119, 119, 1),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
